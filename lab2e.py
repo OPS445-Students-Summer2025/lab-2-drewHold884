2792860f92acdbf54113407847d2fe9af62e9a1a
Andrew Holder <aholder5@myseneca.ca>
2025-06-11 15:34:24 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Andrew Holder
+#Author ID: 146631213
+#Date Created: 2025/05/28
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')  
