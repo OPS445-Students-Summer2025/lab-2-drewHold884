a9e3016a1d8dd234a3b55117c61d5064a20df967
Andrew Holder <aholder5@myseneca.ca>
2025-06-11 15:35:53 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Andrew Holder
+#Author ID: 146631213
+#Date Created: 2025/05/28
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')  
